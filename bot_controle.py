5249f6c9840ddfc93cb6af5439146f1bb9b8be9c
Ember2113 <olegsteam@bk.ru>
2020-06-13 16:07:33 +0300

    Add quit

--- bot_controle.py
+++ bot_controle.py
@@ -7,21 +7,27 @@ class BotControl:
         self.map = map_storage
         self.usb_connection = None  # TODO
         self.scanner_thread = None  # TODO thread to get data from scanner, compute them and map update
+        self.cont = True
 
     def start(self):
-        command = self.get_command()
-        self.perform_command(command)
+        while self.cont:
+            command = self.get_command()
+            self.perform_command(command)
 
     def perform_command(self, command):
+        print command
         sequence_of_bits = command.get_command_bit_sequence()
         if command.command_name == "rotate":
             self.perform_rotate(sequence_of_bits)
         if command.command_name == "move_forward":
             self.perform_moving_forward(sequence_of_bits)
+        if command.command_name == "quit":
+            self.cont = False
 
     def perform_rotate(self, sequence_of_bits):
         angle = sequence_of_bits[1]
         print("performing rotation on %s degrees" % angle)
+
         # TODO usb_connection.send_command(sequence_of_bits)
 
     def perform_moving_forward(self, sequence_of_bits):
