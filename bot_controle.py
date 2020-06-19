2ae9ecabffe2010b2a0d4928ff1c00c15c937d05
VSemikoz <slava.semik@yandex.ru>
2020-06-19 17:44:53 +0300

    code refactor

--- bot_controle.py
+++ bot_controle.py
@@ -1,52 +1,45 @@
 import map_storage
-import serial   # library for port
-import time   #library for time delays
+import serial
+import time
 from command_string import CommandString as CS
 
+UTF8 = "utf-8"
 
 class BotControl:
     def __init__(self):
         self.map = map_storage
-        self.usb_connection = serial.Serial('/dev/ttyUSB0', 9600)  # port for rasspberry pi, on windows can not work
+        self.usb_connection = serial.Serial('/dev/ttyUSB0', 9600)
         self.scanner_thread = None  # TODO thread to get data from scanner, compute them and map update
         self.cont = True
 
+    def initialize(self):
+        self.usb_connection.open()
+
     def start(self):
+        self.initialize()
         while self.cont:
-            usb = self.usb_connection
             command = self.get_command()
             self.perform_command(command)
-            usb.open  # open usb connection(port)
 
-    def perform_command(self, command, usb):
-        print command
+    def perform_command(self, command):
         sequence_of_bits = command.get_command_bit_sequence()
-        if command.command_name == "rotate":
-            x = "2"   # the number by which Arduino understands that she will need to turn
-            usb.write(x.encode())   # sending command numbers to arduino
-            time.sleep(1)  # 1 second pause for correct operation
+        if command.command_name == "rotate" and len(command.parameters) == 1:
+            print("performing rotation on %s degrees" % command.parameters[0])
             self.perform_rotate(sequence_of_bits)
-        if command.command_name == "move_forward":
-            x = "3"  # the number by which Arduino understands that she will need to go right before the obstacle
-            usb.write(x.encode())  # sending command numbers to arduino
+        if command.command_name == "move_forward" and len(command.parameters) == 1:
+            print("performing moving on %s" % command.parameters[0])
             self.perform_moving_forward(sequence_of_bits)
         if command.command_name == "quit":
             self.cont = False
 
-    def perform_rotate(self, sequence_of_bits, usb):
-        angle = sequence_of_bits[1]
-        usb.write(angle.encode())
-        print("performing rotation on %s degrees" % angle)
-
-
+    def perform_rotate(self, sequence_of_bits):
+        self.usb_connection.write(sequence_of_bits.encode())# example_sequence: 1/90
+        time.sleep(.1)
         # TODO usb_connection.send_command(sequence_of_bits)
 
-    def perform_moving_forward(self, sequence_of_bits, usb):
-        distance = sequence_of_bits[1]
-        print("performing moving on %s" % distance)
-        while True:
-            data = usb.readline()
-            print(data)
+    def perform_moving_forward(self, sequence_of_bits):
+        self.usb_connection.write(sequence_of_bits.encode()) #example_sequence: 2/20
+        time.sleep(.1)
         # TODO usb_connection.send_command(sequence_of_bits)
 
     @staticmethod
